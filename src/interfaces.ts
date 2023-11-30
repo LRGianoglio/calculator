0858ef3e4970179e30a61d8c425c05e32b2e3f33
LRGianoglio <luchogianoglio@gmail.com>
2023-11-30 15:24:52 -0300

    finished

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,5 @@
 export interface InitialState {
   screen: string;
   resultScreen: string;
+  reset: boolean;
 }
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,53 +1,90 @@
 import { NUMBER_PRESS, OPERATION_PRESS, POINT_PRESS, AC_PRESS, EQUAL_PRESS, CalculatorActions } from './types';
 import { InitialState } from '../interfaces';
 
-
 const initialState: InitialState = {
   screen: "",
-  resultScreen: "0"
+  resultScreen: "0",
+  reset: false
 };
 
-const todoReducer = (state = initialState, action: CalculatorActions): InitialState => {
+const calculatorReducer = (state = initialState, action: CalculatorActions): InitialState => {
   switch (action.type) {
     case NUMBER_PRESS:
-      if (action.payload === "0" && state.screen==="0") return {...state}
+      if (state.screen === "0" || state.reset) {
+        return { ...state, screen: action.payload, reset: false };
+      }
       return {
         ...state,
         screen: state.screen + action.payload,
       };
-    case OPERATION_PRESS:
-      return{
-        ...state,
-        resultScreen: action.payload
-      }
-    case POINT_PRESS:
-      return{
-        ...state,
-        resultScreen: "POINT"
-      }
+      case OPERATION_PRESS:
+        return {
+          ...state,
+          screen: state.screen + action.payload,
+          reset: false,
+        };
+        case POINT_PRESS:
+          const lastOperand = state.screen.split(/([+\-x/])/).pop();
+          if (!lastOperand?.includes('.')) {
+            return {
+              ...state,
+              screen: state.screen + '.',
+              reset: false,
+            };
+          }
+          return state;
     case AC_PRESS:
-      return{
-        ...state,
-        resultScreen: "AC"
-      }
+      return {
+        ...initialState,
+      };
     case EQUAL_PRESS:
       const arrayedfiedOperation: string[] = state.screen.split(/([+\-x/])/).filter(token => token.trim() !== '');
-      let endResult:number | string = 666;
-      if (arrayedfiedOperation.length === 1) {
-        endResult = parseFloat(arrayedfiedOperation[0]) + 0
-      } else {
-        for (let i:number = 0; i<arrayedfiedOperation.length; i++){
+      let endResult: number | string = 0;
 
+      if (arrayedfiedOperation.length === 1) {
+        endResult = parseFloat(arrayedfiedOperation[0]);
+      } else {
+        let operator = '+';
+        let midResult: number = 0;
+
+        for (let i: number = 0; i < arrayedfiedOperation.length; i++) {
+          const token = arrayedfiedOperation[i];
+
+          if (['+', '-', 'x', '/'].includes(token)) {
+            operator = token;
+          } else {
+            const num = parseFloat(token);
+
+            switch (operator) {
+              case '+':
+                midResult += num;
+                break;
+              case '-':
+                midResult -= num;
+                break;
+              case 'x':
+                midResult *= num;
+                break;
+              case '/':
+                midResult /= num;
+                break;
+              default:
+                break;
+            }
+          }
         }
-      };
-      
-      return{
-        ...state,
-        resultScreen: endResult.toString()
+
+        endResult = midResult;
       }
+
+      return {
+        ...state,
+        resultScreen: endResult.toString(),
+        reset: true,
+      };
     default:
-      return {...state};
+      return { ...state };
   }
 };
 
-export default todoReducer;
+export default calculatorReducer;
